4e1856246089c9a33393ae50088b0b24f5288ed4
Doug Hellmann <doug@doughellmann.com>
2014-03-23 11:17:21 -0400

    use pbr for packaging

--- setup.py
+++ setup.py
@@ -1,48 +1,8 @@
-# -*- coding: utf-8 -*-
+#!/usr/bin/env python
 
-from setuptools import setup, find_packages
-
-try:
-    long_desc = open('README.rst', 'r').read()
-except IOError:
-    long_desc = '''
-This package contains an alternative integration of Sphinx and
-Paver allowing for both HTML and PDF generation from the same
-pavement.py file.
-'''
-
-requires = ['Sphinx>=0.6', 'Paver>=1.02.0']
-
-NAME = 'sphinxcontrib-paverutils'
-VERSION = '1.5'
+from setuptools import setup
 
 setup(
-    name=NAME,
-    version=VERSION,
-    url='https://bitbucket.org/dhellmann/sphinxcontrib-paverutils/',
-    license='BSD',
-    author='Doug Hellmann',
-    author_email='doug@doughellmann.com',
-    description='Sphinx/Paver integration',
-    long_description=long_desc,
-    zip_safe=False,
-    classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        'Environment :: Console',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: BSD License',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
-        'Topic :: Documentation',
-        'Topic :: Utilities',
-    ],
-    platforms='any',
-    packages=find_packages(),
-    include_package_data=True,
-    install_requires=requires,
-    namespace_packages=['sphinxcontrib'],
+    setup_requires=['pbr'],
+    pbr=True,
 )
